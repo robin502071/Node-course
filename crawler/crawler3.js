b43a820e53a75970d8e484d87274ace6f5947069
robin <robininispan@gmail.com>
2022-05-15 17:14:30 +0800

    add crawler3.js

---
+++ crawler/crawler3.js
@@ -0,0 +1,25 @@
+// read stock no from stock.txt
+
+// npm i axios
+const axios = require('axios');
+const fs = require('fs/promises');
+
+(async function () {
+  try {
+    const stockText = await fs.readFile('./stock.txt', 'utf-8');
+    const response = await axios.get(
+      'https://www.twse.com.tw/exchangeReport/STOCK_DAY',
+      {
+        params: {
+          // 設定 query string
+          response: 'json',
+          date: '20220301',
+          stockNo: stockText,
+        },
+      }
+    );
+    console.log(response.data);
+  } catch (err) {
+    console.log(err);
+  }
+})();
