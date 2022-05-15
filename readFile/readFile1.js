170c149bd7b87d4756dd428fb1bad3857d079909
robin <robininispan@gmail.com>
2022-05-15 11:17:18 +0800

    add readFile1.js

---
+++ readFile/readFile1.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+
+const p = new Promise((resolve, reject) => {
+  fs.readFile('./readMePls.txt', 'utf-8', (err, data) => {
+    if (err) {
+      // 錯誤了
+      reject(err);
+      // console.log('喔喔喔，發生錯誤了');
+      // console.error(err);
+    } else {
+      // 因為沒有 err，所以是正確的
+      resolve(data);
+      // console.log(data);
+    }
+  });
+});
+
+p.then((data) => {
+  console.log('成功了');
+  console.log(data);
+}).catch((err) => {
+  console.log(err);
+});
