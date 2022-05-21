0d4207d26f6a13fb8d78764cdc654d4f6056719c
robin <robininispan@gmail.com>
2022-05-21 14:26:36 +0800

    add .env / .env.example / modified crawler4.js

--- crawler/crawler4.js
+++ crawler/crawler4.js
@@ -1,11 +1,12 @@
 const mysql = require('mysql2/promise');
+require('dotenv').config();
 
 (async () => {
   const connection = await mysql.createConnection({
-    host: 'localhost',
-    user: 'weiwei',
-    password: '',
-    database: 'nodejscourse',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   });
 
   // query database
