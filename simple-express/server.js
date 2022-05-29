b7f6a211738702eb0a432fd7aaaca4dad7e3ac6c
robin <robininispan@gmail.com>
2022-05-29 12:26:02 +0800

    added pagination expression but not finished

--- simple-express/server.js
+++ simple-express/server.js
@@ -26,9 +26,28 @@ app.get('/stocks', async (request, response, next) => {
 // 取得單一股票，:stockId 是一個變數
 app.get('/stocks/:stockId', async (request, response, next) => {
   let [data, fields] = await pool.execute(
-    'SELECT * FROM stocks WHERE id =' + request.params.stockId
+    'SELECT * FROM stock_prices WHERE stock_id = ?',
+    [request.params.stockId]
   );
-  response.json(data);
+
+  // RESTful 風格之下鼓勵把過濾參數用 query string 來傳遞
+  // /stocks/:stockId?page=1
+  // 取得目前在第幾頁並且給預設值為 1
+  let page = request.query.page || 1;
+
+  // 取得目前的總筆數
+  const total = data.length;
+  // 計算總共有幾頁
+  const perPage = 5; // 每頁幾筆
+  const lastPage = Math.ceil(total / perPage);
+
+  // 計算 offsect 是多少 => 要跳過幾筆
+  let offset = (page - 1) * perPage;
+  console.log(offset);
+  response.json({
+    paginaton: {},
+    data,
+  });
 });
 
 app.listen(3001, () => {
